d2afe0c5c79e8fba2698ee8c3ec964b29a1ed98c
José Garção <josemariagarcao15@gmail.com>
2022-11-10 19:58:12 +0000

    restore package.json

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const { readFile } = require("fs");
 
+console.log("started first task");
+// CHECK FILE PATH!!!
+readFile("./content/first.txt", "utf8", (err, result) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(result);
+    console.log("completed first task");
+});
+console.log("starting next task");
