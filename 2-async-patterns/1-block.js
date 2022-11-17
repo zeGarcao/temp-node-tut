8289d8990b4cd12fe024f574d4f567e2c688adc3
José Garção <josemariagarcao15@gmail.com>
2022-11-17 16:11:29 +0000

    update async patterns

---
+++ 2-async-patterns/1-block.js
@@ -0,0 +1,20 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.end("Home Page");
+    } else if (req.url === "/about") {
+        for (let i = 0; i < 1000; ++i) {
+            for (let j = 0; j < 1000; ++j) {
+                console.log(`${i} ${j}`);
+            }
+        }
+        res.end("About Page");
+    } else {
+        console.log("Error Page");
+    }
+});
+
+server.listen(5000, () => {
+    console.log("Server listening on port 5000...");
+});
--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-const http = require("http");
+const { readFile, writeFile } = require("fs").promises;
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
-const server = http.createServer((req, res) => {
-    console.log("request event");
-    res.end("Hello world");
-});
+const start = async () => {
+    try {
+        const first = await readFile("./content/first.txt", "utf-8");
+        const second = await readFile("./content/second.txt", "utf-8");
+        await writeFile(
+            "./content/result-mind-grenade.txt",
+            `THIS IS AWESOME : ${first} ${second}`,
+            { flag: "a" }
+        );
+        console.log(first, second);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
-server.listen(5000, () => {
-    console.log("Server listening on port : 5000....");
-});
+start();
+
+// const getText = path => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, "utf-8", (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             } else {
+//                 resolve(data);
+//             }
+//         });
+//     });
+// };
+
+// getText("./content/first.txt")
+//     .then(result => console.log(result))
+//     .catch(err => console.log(err));
