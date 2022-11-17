c59d9cf577bc8c9332ee25251aa61958b713a51f
José Garção <josemariagarcao15@gmail.com>
2022-11-17 16:37:31 +0000

    update event emitter and request event files

--- app.js
+++ app.js
@@ -1,37 +1,15 @@
-const { readFile, writeFile } = require("fs").promises;
-// const util = require("util");
-// const readFilePromise = util.promisify(readFile);
-// const writeFilePromise = util.promisify(writeFile);
+const http = require("http");
 
-const start = async () => {
-    try {
-        const first = await readFile("./content/first.txt", "utf-8");
-        const second = await readFile("./content/second.txt", "utf-8");
-        await writeFile(
-            "./content/result-mind-grenade.txt",
-            `THIS IS AWESOME : ${first} ${second}`,
-            { flag: "a" }
-        );
-        console.log(first, second);
-    } catch (error) {
-        console.log(error);
-    }
-};
+// const server = http.createServer((req, res) => {
+//     res.end("Welcome");
+// });
 
-start();
+// Using Event Emitter API
+const server = http.createServer();
+// emits request event
+// subscribe to it / listen for it / respond to it
+server.on("request", (req, res) => {
+    res.end("Welcome");
+});
 
-// const getText = path => {
-//     return new Promise((resolve, reject) => {
-//         readFile(path, "utf-8", (err, data) => {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve(data);
-//             }
-//         });
-//     });
-// };
-
-// getText("./content/first.txt")
-//     .then(result => console.log(result))
-//     .catch(err => console.log(err));
+server.listen(5000);
